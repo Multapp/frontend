d5bf0cea05d731b1ff7c7d5ab0e1ddb372492697
EdgarCardozo <edgar07cardozo@hotmail.com>
2022-05-29 19:42:56 -0300

    Fix recuperar contra, agrega rol

--- src/containers/Login/actions.js
+++ src/containers/Login/actions.js
@@ -62,6 +62,7 @@ export const recuperarContrasena = (email) => {
         const data = {
             email: email,
             returnSecureToken: true,
+            rol: "Otro"
         }
         const headers = {
             "content-type": "application/json",
